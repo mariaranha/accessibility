9a8ec4383c93158fd1b277fcdc54b27a62de6f9b
Matheus Oliveira <matheusdesouzaoliveira@hotmail.com>
2020-01-31 13:42:33 -0300

    homePopulation merge

--- Accessibility/Accessibility/Controllers/Home/HomeViewController.swift
+++ Accessibility/Accessibility/Controllers/Home/HomeViewController.swift
@@ -13,11 +13,23 @@ class HomeViewController: UIViewController, UICollectionViewDataSource, UICollec
     let weekDays = ["S", "T", "Q", "Q", "S", "S", "D"]
     let days = ["27", "28", "29", "30", "31", "01", "02", "03", "04", "05"]
     
+    var monthDay: Int!
+    var json: [Discipline] = []
+    var disciplinesOfTheDay: [Discipline] = []
+    var sportsOfTheDay: [String] = []
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.monthDay = getMonthDay()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(false)
+        loadJSONFile()
+        loadSportsOfTheDay(day: self.monthDay)
+        printSportsOfTheDay()
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -33,6 +45,73 @@ class HomeViewController: UIViewController, UICollectionViewDataSource, UICollec
         }
         
         return UICollectionViewCell()
+    }
+    
+    private func getMonthDay() -> Int {
+        // Return the user current month day
+        let date = Date()
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.day], from: date)
+        let monthDay = components.day
+        return monthDay ?? 32
+    }
+    
+    private func printSportsOfTheDay() {
+        
+        // Removing duplicated sports
+        
+        let unique = Array(Set(self.sportsOfTheDay)).sorted()
+        
+        for sport in unique {
+            print(sport)
+        }
+    }
+    
+    // MARK: - JSON Functions
+    
+    private func loadJSONFile() {
+        // Loading JSON file and building and building JSON array.
+        if let url = Bundle.main.url(forResource: "calendar", withExtension: "json") {
+            do {
+                let jsonData = try Data(contentsOf: url, options: .mappedIfSafe)
+                do {
+                    if let jsonResult = try JSONSerialization.jsonObject(with: jsonData,
+                                        options: JSONSerialization.ReadingOptions(rawValue: 0)) as? NSDictionary {
+                        if let disciplinesArray = jsonResult.value(forKey: "discipline") as? NSArray {
+                            for (_, element) in disciplinesArray.enumerated() {
+                                if let element = element as? NSDictionary {
+                                    
+                                    let discipline = Discipline(json: element as! [String : Any] )
+                                    self.json.append(discipline!)
+                                }
+                            }
+                        }
+                    }
+                } catch let error as NSError {
+                    print("Error: \(error)")
+                }
+            } catch let error as NSError {
+                print("Error: \(error)")
+            }
+        }
+    }
+    
+    private func loadSportsOfTheDay(day: Int) {
+        for discipline in self.json {
+            for disciplineDay in discipline.normalDates {
+                if disciplineDay == day {
+                    self.disciplinesOfTheDay.append(discipline)
+                    self.sportsOfTheDay.append(discipline.sport)
+                }
+            }
+            
+            for disciplineDay in discipline.medalDates {
+                if disciplineDay  == day {
+                    self.disciplinesOfTheDay.append(discipline)
+                    self.sportsOfTheDay.append(discipline.sport)
+                }
+            }
+        }
     }
 
 
