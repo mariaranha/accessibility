dd11bbbd2a7441e9d41f1673acd8dee23a7e4c60
Matheus Oliveira <matheusdesouzaoliveira@hotmail.com>
2020-02-03 08:50:18 -0300

    Rafactoring JSON functions

---
+++ Accessibility/AccessibilityTests/JSONManager.swift
@@ -0,0 +1,44 @@
+//
+//  JSONManager.swift
+//  Accessibility
+//
+//  Created by Matheus Oliveira on 03/02/20.
+//  Copyright © 2020 Marina Miranda Aranha. All rights reserved.
+//
+
+import Foundation
+
+class JSONManager {
+    
+    private var json: [Discipline] = []
+    
+    public func loadJSONFile() -> [Discipline] {
+        // Loading JSON file and building and building JSON array.
+        if let url = Bundle.main.url(forResource: "calendar", withExtension: "json") {
+            do {
+                let jsonData = try Data(contentsOf: url, options: .mappedIfSafe)
+                do {
+                    if let jsonResult = try JSONSerialization.jsonObject(with: jsonData,
+                                        options: JSONSerialization.ReadingOptions(rawValue: 0)) as? NSDictionary {
+                        if let disciplinesArray = jsonResult.value(forKey: "discipline") as? NSArray {
+                            for (_, element) in disciplinesArray.enumerated() {
+                                if let element = element as? NSDictionary {
+                                    
+                                    let discipline = Discipline(json: element as! [String : Any] )
+                                    self.json.append(discipline!)
+                                }
+                            }
+                        }
+                    }
+                } catch let error as NSError {
+                    print("Error: \(error)")
+                }
+            } catch let error as NSError {
+                print("Error: \(error)")
+            }
+        }
+        return json
+    }
+    
+    
+}
