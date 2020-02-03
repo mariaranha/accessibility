87193347d07da58901caf93bf3ca1d7f76b8605e
Marina Miranda Aranha <ra174233@students.ic.unicamp.br>
2020-02-03 10:59:34 -0300

    add sports cards

--- Accessibility/Accessibility/Controllers/Home/HomeViewController.swift
+++ Accessibility/Accessibility/Controllers/Home/HomeViewController.swift
@@ -11,30 +11,89 @@ import UIKit
 class HomeViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     let weekDays = ["S", "T", "Q", "Q", "S", "S", "D"]
-    let days = ["27", "28", "29", "30", "31", "01", "02", "03", "04", "05"]
+    let days = ["22", "23", "24", "25", "26", "27", "28", "29", "30", "31",
+                "01", "02", "03", "04", "05", "06", "07", "08", "09"]
+    
+    var screenSize: CGRect!
+    var screenWidth: CGFloat!
+    var screenHeight: CGFloat!
     
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var calendarCollectionView: UICollectionView!
+    @IBOutlet weak var dayLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return days.count
+        if collectionView == calendarCollectionView {
+            return days.count
+        } else {
+            return 5
+        }
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CalendarCollectionViewCell.reuseIdentifier,
-        for: indexPath) as? CalendarCollectionViewCell {
-            let day = days[indexPath.row]
-            cell.configureCell(weekDay: "S", day: day)
-            return cell
+        
+        if collectionView == calendarCollectionView {
+            if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CalendarCollectionViewCell.reuseIdentifier,
+            for: indexPath) as? CalendarCollectionViewCell {
+                let day = days[indexPath.row]
+                let weekDay = weekDays[indexPath.row % 7]
+                cell.configureCell(weekDay: weekDay, day: day)
+                
+                if Int(day) == 22{
+                    collectionView.selectItem(at: indexPath, animated: true, scrollPosition: UICollectionView.ScrollPosition.centeredHorizontally)
+                    
+                } else {
+                    cell.selectDay(isSelected: false)
+                }
+                
+                return cell
+            }
+        } else {
+            if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.reuseIdentifier, for: indexPath) as? CollectionViewCell {
+                           
+                           
+                return cell
+            }
         }
         
         return UICollectionViewCell()
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if collectionView == calendarCollectionView {
+            if let cell = collectionView.cellForItem(at: indexPath) as? CalendarCollectionViewCell {
+                cell.selectDay(isSelected: true)
+                var month: String!
+                var day = Int(cell.dayLabel.text!)!
+                if day < 10 {
+                    month = "Agosto"
+                } else {
+                    month = "Julho"
+                }
+                self.dayLabel.text = "\(day) de \(month!) de 2020"
 
+            }
+        } else {
+            
+            
+            
+        }
+        
+
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        if collectionView == calendarCollectionView{
+            if let cell = collectionView.cellForItem(at: indexPath) as? CalendarCollectionViewCell {
+                cell.selectDay(isSelected: false)
+
+            }
+        } 
+    }
 
 }
 
