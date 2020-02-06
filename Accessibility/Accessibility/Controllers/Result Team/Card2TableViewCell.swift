1e687dba1bf0dc4227738f2707246aaa41a5cafb
Matheus Oliveira <matheusdesouzaoliveira@hotmail.com>
2020-02-06 09:00:02 -0300

    Merging develop with result branch

---
+++ Accessibility/Accessibility/Controllers/Result Team/Card2TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  Card2TableViewCell.swift
+//  Accessibility
+//
+//  Created by Felipe Luna Tersi on 01/02/20.
+//  Copyright © 2020 Marina Miranda Aranha. All rights reserved.
+//
+
+import UIKit
+
+class Card2TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Accessibility/Accessibility/Controllers/Result Team/Result2TableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  Result2TableViewCell.swift
+//  Accessibility
+//
+//  Created by Felipe Luna Tersi on 01/02/20.
+//  Copyright © 2020 Marina Miranda Aranha. All rights reserved.
+//
+
+import UIKit
+
+class Result2TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        title.isAccessibilityElement = true
+        title.accessibilityHint = "título"
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
