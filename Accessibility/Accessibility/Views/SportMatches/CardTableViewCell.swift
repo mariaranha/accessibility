573c65684e2775b29aac47c7c8f35c3b3a28942e
Matheus de Souza Oliveira <matheus@Matheuss-MacBook-Pro.local>
2020-02-12 22:06:16 -0300

    aaaa

--- Accessibility/Accessibility/Views/SportMatches/CardTableViewCell.swift
+++ Accessibility/Accessibility/Views/SportMatches/CardTableViewCell.swift
@@ -15,117 +15,117 @@ class CardTableViewCell: UITableViewCell {
     
     @IBOutlet weak var cardViewDynamic: CardViewDynamic!
     
-   override func awakeFromNib() {
-            super.awakeFromNib()
-            
-            if cardView?.contentView != nil {
-                cardView?.contentView.layer.cornerRadius = 8
-            }
-            
-            if cardViewDynamic?.contentView != nil {
-                cardViewDynamic?.contentView.layer.cornerRadius = 8
-            }
-            
-        }
+ override func awakeFromNib() {
+           super.awakeFromNib()
+           
+           if cardView?.contentView != nil {
+               cardView?.contentView.layer.cornerRadius = 8
+           }
+           
+           if cardViewDynamic?.contentView != nil {
+               cardViewDynamic?.contentView.layer.cornerRadius = 8
+           }
+           
+       }
 
-        override func setSelected(_ selected: Bool, animated: Bool) {
-            super.setSelected(selected, animated: animated)
-            if cardView?.contentView != nil {
-                setCardViewVoiceOver()
-            }
-            
-            if cardViewDynamic?.contentView != nil {
-                setCardViewDynamicVoiceOver()
-            }
-        }
+       override func setSelected(_ selected: Bool, animated: Bool) {
+           super.setSelected(selected, animated: animated)
+           if cardView?.contentView != nil {
+               setCardViewVoiceOver()
+           }
+           
+           if cardViewDynamic?.contentView != nil {
+               setCardViewDynamicVoiceOver()
+           }
+       }
 
-        func configure(with viewModel: CardView.ViewModel) {
-            cardView?.configure(with: viewModel)
-        }
-        
-        func configureDynamic(with viewModel: CardViewDynamic.ViewModel) {
-            cardViewDynamic?.configure(with: viewModel)
-        }
-    }
+       func configure(with viewModel: CardView.ViewModel) {
+           cardView?.configure(with: viewModel)
+       }
+       
+       func configureDynamic(with viewModel: CardViewDynamic.ViewModel) {
+           cardViewDynamic?.configure(with: viewModel)
+       }
+   }
 
-    // MARK: Voice Over
-    extension CardTableViewCell {
-        func setCardViewVoiceOver() {
-            let title = cardView?.titleLabel?.text
-            let subtitle = cardView?.subtitleLabel?.text
-            let points = cardView?.numberOfPointsLabel?.text
-            let day = cardView?.dayLabel?.text
-            let month = cardView?.monthLabel?.text
-            let time = cardView?.timeLabel?.text
-            let firstCountry = cardView?.firstCountryLabel?.text
-            let secondCountry = cardView?.secondCountryLabel?.text
-            let firstButton = cardView?.firstCountryButton
-            let secondButton = cardView?.secondCountryButton
-            
-            cardView?.isAccessibilityElement = true
-            firstButton?.isAccessibilityElement = true
-            secondButton?.isAccessibilityElement = true
-            
-            var cardAccessInfo = ""
-            
-            if points != "" && points != "–" && points != nil {
-                
-                cardAccessInfo = String(format: NSLocalizedString("Match ended %@, %@, %@ %@ at %@. Brazil versus China. You did %@ points", comment: "Card da partida com todas as informações: Título, subtítulo, dia, mês, hora, países, pontuação"), title ?? "", subtitle ?? "", day ?? "", month ?? "", time ?? "", points ?? "")
-                
-                firstButton?.isAccessibilityElement = false
-                secondButton?.isAccessibilityElement = false
-            } else {
-                if firstCountry != "???" || secondCountry != "???" {
-                    
-                    cardAccessInfo = String(format: NSLocalizedString("Match %@. %@. %@ %@ at %@. Brazil versus China. Select a country to throb or double tap for more information", comment: "Card da partida com todas as informações: Título, subtítulo, dia, mês, hora, países"), title ?? "", subtitle ?? "", day ?? "", month ?? "", time ?? "")
+   // MARK: Voice Over
+   extension CardTableViewCell {
+       func setCardViewVoiceOver() {
+           let title = cardView?.titleLabel?.text
+           let subtitle = cardView?.subtitleLabel?.text
+           let points = cardView?.numberOfPointsLabel?.text
+           let day = cardView?.dayLabel?.text
+           let month = cardView?.monthLabel?.text
+           let time = cardView?.timeLabel?.text
+           let firstCountry = cardView?.firstCountryLabel?.text
+           let secondCountry = cardView?.secondCountryLabel?.text
+           let firstButton = cardView?.firstCountryButton
+           let secondButton = cardView?.secondCountryButton
+           
+           cardView?.isAccessibilityElement = true
+           firstButton?.isAccessibilityElement = true
+           secondButton?.isAccessibilityElement = true
+           
+           var cardAccessInfo = ""
+           
+           if points != "" && points != "–" && points != nil {
+               
+               cardAccessInfo = String(format: NSLocalizedString("Match ended %@, %@, %@ %@ at %@. Brazil versus China. You did %@ points", comment: "Card da partida com todas as informações: Título, subtítulo, dia, mês, hora, países, pontuação"), title ?? "", subtitle ?? "", day ?? "", month ?? "", time ?? "", points ?? "")
+               
+               firstButton?.isAccessibilityElement = false
+               secondButton?.isAccessibilityElement = false
+           } else {
+               if firstCountry != "???" || secondCountry != "???" {
+                   
+                   cardAccessInfo = String(format: NSLocalizedString("Match %@. %@. %@ %@ at %@. Brazil versus China. Select a country to throb or double tap for more information", comment: "Card da partida com todas as informações: Título, subtítulo, dia, mês, hora, países"), title ?? "", subtitle ?? "", day ?? "", month ?? "", time ?? "")
 
-                    firstButton?.accessibilityHint = String(format: NSLocalizedString("Double tap to throb at Brazil", comment: ""))
-                    secondButton?.accessibilityHint = String(format: NSLocalizedString("Double tap to throb at China", comment: ""))
-                    
-                } else {
-                    
-                    cardAccessInfo = String(format: NSLocalizedString("Partida %@, %@, dia %@ de %@ às %@. Países não definidos", comment: "Card da partida com todas as informações: Título, subtítulo, dia, mês, hora, países"), title ?? "", subtitle ?? "", day ?? "", month ?? "", time ?? "")
-                    
-                }
-            }
-        
-            cardView?.accessibilityLabel = cardAccessInfo
-            
-            accessibilityElements = [cardView as Any, firstButton as Any, secondButton as Any]
-        }
-        
-        func setCardViewDynamicVoiceOver() {
-            let title = cardViewDynamic?.titleLabel?.text
-            let subtitle = cardViewDynamic?.subtitleLabel?.text
-            let points = cardViewDynamic?.numberOfPointsLabel?.text
-            let date = cardViewDynamic.dateLabel?.text
-            let firstCountry = cardViewDynamic?.firstCountryLabel?.text
-            let secondCountry = cardViewDynamic?.secondCountryLabel?.text
-            let firstButton = cardViewDynamic?.firstCountryButton
-            let secondButton = cardViewDynamic?.secondCountryButton
-            
-            cardViewDynamic?.isAccessibilityElement = true
-            firstButton?.isAccessibilityElement = true
-            secondButton?.isAccessibilityElement = true
-            
-            var cardAccessInfo = ""
-            
-            if points != "" && points != "–" && points != nil {
-                
-                cardAccessInfo = String(format: NSLocalizedString("Match ended. %@. %@. %@. %@ versus %@.", comment: ""), title ?? "", subtitle ?? "", date ?? "", firstCountry ?? "", secondCountry ?? "")
-            } else {
-                if firstCountry != "???" {
-                    cardAccessInfo = String(format: NSLocalizedString("Match %@. %@. %@ %@ at %@. Brazil versus China. Select a country to throb or double tap for more information", comment: "Match card with informations: Title, subtitle, date and countries"), title ?? "", subtitle ?? "", date ?? "")
-                    
-                    firstButton?.accessibilityHint = NSLocalizedString("Clique duas vezes para palpitar em Brazil", comment: "")
-                    secondButton?.accessibilityHint = NSLocalizedString("Clique duas vezes para palpitar em China)", comment: "")
-                } else {
-                    cardAccessInfo = String(format: NSLocalizedString("Match. %@. %@. %@. Undefinied countries", comment: ""),title ?? "", subtitle ?? "", date ?? "" )
-                }
-            }
-        
-            cardViewDynamic?.accessibilityLabel = cardAccessInfo
-            
-            accessibilityElements = [cardViewDynamic as Any, firstButton as Any, secondButton as Any]
-        }
-    }
+                   firstButton?.accessibilityHint = String(format: NSLocalizedString("Double tap to throb at Brazil", comment: ""))
+                   secondButton?.accessibilityHint = String(format: NSLocalizedString("Double tap to throb at China", comment: ""))
+                   
+               } else {
+                   
+                   cardAccessInfo = String(format: NSLocalizedString("Partida %@, %@, dia %@ de %@ às %@. Países não definidos", comment: "Card da partida com todas as informações: Título, subtítulo, dia, mês, hora, países"), title ?? "", subtitle ?? "", day ?? "", month ?? "", time ?? "")
+                   
+               }
+           }
+       
+           cardView?.accessibilityLabel = cardAccessInfo
+           
+           accessibilityElements = [cardView as Any, firstButton as Any, secondButton as Any]
+       }
+       
+       func setCardViewDynamicVoiceOver() {
+           let title = cardViewDynamic?.titleLabel?.text
+           let subtitle = cardViewDynamic?.subtitleLabel?.text
+           let points = cardViewDynamic?.numberOfPointsLabel?.text
+           let date = cardViewDynamic.dateLabel?.text
+           let firstCountry = cardViewDynamic?.firstCountryLabel?.text
+           let secondCountry = cardViewDynamic?.secondCountryLabel?.text
+           let firstButton = cardViewDynamic?.firstCountryButton
+           let secondButton = cardViewDynamic?.secondCountryButton
+           
+           cardViewDynamic?.isAccessibilityElement = true
+           firstButton?.isAccessibilityElement = true
+           secondButton?.isAccessibilityElement = true
+           
+           var cardAccessInfo = ""
+           
+           if points != "" && points != "–" && points != nil {
+               
+               cardAccessInfo = String(format: NSLocalizedString("Match ended. %@. %@. %@. %@ versus %@.", comment: ""), title ?? "", subtitle ?? "", date ?? "", firstCountry ?? "", secondCountry ?? "")
+           } else {
+               if firstCountry != "???" {
+                   cardAccessInfo = String(format: NSLocalizedString("Match %@. %@. %@ %@ at %@. Brazil versus China. Select a country to throb or double tap for more information", comment: "Match card with informations: Title, subtitle, date and countries"), title ?? "", subtitle ?? "", date ?? "")
+                   
+                   firstButton?.accessibilityHint = NSLocalizedString("Clique duas vezes para palpitar em Brazil", comment: "")
+                   secondButton?.accessibilityHint = NSLocalizedString("Clique duas vezes para palpitar em China)", comment: "")
+               } else {
+                   cardAccessInfo = String(format: NSLocalizedString("Match. %@. %@. %@. Undefinied countries", comment: ""),title ?? "", subtitle ?? "", date ?? "" )
+               }
+           }
+       
+           cardViewDynamic?.accessibilityLabel = cardAccessInfo
+           
+           accessibilityElements = [cardViewDynamic as Any, firstButton as Any, secondButton as Any]
+       }
+   }
--- Accessibility/Accessibility/Views/SportMatches/CardView.swift
+++ Accessibility/Accessibility/Views/SportMatches/CardView.swift
@@ -30,142 +30,143 @@ final class CardView: UIView {
     @IBOutlet weak var firstCountryButton: UIButton!
     @IBOutlet weak var secondCountryButton: UIButton!
     
-        //Bet variables
-        var isFirstCountrySelected: Bool = false
-        var isSecondCountrySelected: Bool = false
-        let widthMultiplier: CGFloat = 1.8
-        
-        override init(frame: CGRect) {
-            super.init(frame: frame)
-            setup()
-        }
-        
-        required init?(coder aDecoder: NSCoder) {
-            super.init(coder: aDecoder)
-            setup()
-        }
-        
-        func setup() {
-            Bundle.main.loadNibNamed("CardView", owner: self, options: nil)
-            addSubview(contentView)
-            contentView.frame = self.bounds
-            contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-            let pointsTitle = NSLocalizedString("Points", comment: "User points in the game")
-            self.pointsLabel?.text = pointsTitle
-        }
-        
-        
-        @IBAction func firstCountrySelected(_ sender: UIButton) {
-            let firstFlagWidth = firstCountryWidth.constant
-            let secondFlagWidth = secondCountryWidth.constant
+     
+            //Bet variables
+            var isFirstCountrySelected: Bool = false
+            var isSecondCountrySelected: Bool = false
+            let widthMultiplier: CGFloat = 1.8
             
-            if isFirstCountrySelected == false && isSecondCountrySelected == false {
-                isFirstCountrySelected = !isFirstCountrySelected
-                secondCountryButton.backgroundColor = .white
-                secondCountryButton.alpha = 0.6
-                
-                firstCountryWidth.constant = firstFlagWidth * widthMultiplier
-                
-            } else if isFirstCountrySelected == true && isSecondCountrySelected == false {
-                isFirstCountrySelected = !isFirstCountrySelected
-                secondCountryButton.backgroundColor = .clear
-                secondCountryButton.alpha = 1.0
-                
-                firstCountryWidth.constant = firstFlagWidth / widthMultiplier
-                
-            } else if isFirstCountrySelected == false && isSecondCountrySelected == true {
-                isFirstCountrySelected = !isFirstCountrySelected
-                isSecondCountrySelected = !isSecondCountrySelected
-                firstCountryButton.backgroundColor = .clear
-                firstCountryButton.alpha = 1.0
-                
-                secondCountryButton.backgroundColor = .white
-                secondCountryButton.alpha = 0.6
-                
-                firstCountryWidth.constant = firstFlagWidth * widthMultiplier
-                secondCountryWidth.constant = secondFlagWidth / widthMultiplier
-            }
-        
-        }
-        
-        
-        @IBAction func secondCountrySelected(_ sender: UIButton) {
-            let firstFlagWidth = firstCountryWidth.constant
-            let secondFlagWidth = secondCountryWidth.constant
-            
-            if isFirstCountrySelected == false && isSecondCountrySelected == false {
-                isSecondCountrySelected = !isSecondCountrySelected
-                firstCountryButton.backgroundColor = .white
-                firstCountryButton.alpha = 0.6
-                
-                secondCountryWidth.constant = secondFlagWidth * widthMultiplier
-                
-            } else if isFirstCountrySelected == false && isSecondCountrySelected == true {
-                isSecondCountrySelected = !isSecondCountrySelected
-                firstCountryButton.backgroundColor = .clear
-                firstCountryButton.alpha = 1.0
-                
-                secondCountryWidth.constant = secondFlagWidth / widthMultiplier
-                
-            } else if isFirstCountrySelected == true && isSecondCountrySelected == false {
-                isSecondCountrySelected = !isSecondCountrySelected
-                isFirstCountrySelected = !isFirstCountrySelected
-                secondCountryButton.backgroundColor = .clear
-                secondCountryButton.alpha = 1.0
-                
-                firstCountryButton.backgroundColor = .white
-                firstCountryButton.alpha = 0.6
-                
-                firstCountryWidth.constant = firstFlagWidth / widthMultiplier
-                secondCountryWidth.constant = secondFlagWidth * widthMultiplier
-            }
-        }
-        
-        func configure(with viewModel: ViewModel) {
-            titleLabel?.text = viewModel.title
-            subtitleLabel?.text = viewModel.subtitle
-            numberOfPointsLabel?.text = viewModel.numberOfPoints
-            firstCountryLabel?.text = viewModel.firstCountryName
-            secondCountryLabel?.text = viewModel.secondCountryName
-            dayLabel?.text = viewModel.day
-            monthLabel?.text = viewModel.mounth
-            timeLabel?.text = viewModel.time
-            
-            cornerImage?.image = viewModel.cornerImage
-            firstCountryImage?.image = viewModel.firstCountryFlag
-            secondCountryImage?.image = viewModel.secondCountryFlag
-        }
-        @IBAction func firstCountryPressed(_ sender: Any) {
-            firstCountryButton.isAccessibilityElement = true
-            if isFirstCountrySelected == false{
-                firstCountryButton?.accessibilityHint = String(format: NSLocalizedString("You throbbed at Brazil", comment: ""))
-            } else {
-                firstCountryButton?.accessibilityHint = String(format: NSLocalizedString("Double tap to throb at Brazil", comment: ""))
-            }
-        }
-        @IBAction func secondCountryPressed(_ sender: Any) {
-            secondCountryButton.isAccessibilityElement = true
-            if isSecondCountrySelected == false{
-                firstCountryButton?.accessibilityHint = String(format: NSLocalizedString("You throbbed at China", comment: ""))
-            } else {
-                firstCountryButton?.accessibilityHint = String(format: NSLocalizedString("Double tap to throb at China", comment: ""))
+            override init(frame: CGRect) {
+                super.init(frame: frame)
+                setup()
             }
             
+            required init?(coder aDecoder: NSCoder) {
+                super.init(coder: aDecoder)
+                setup()
+            }
+            
+            func setup() {
+                Bundle.main.loadNibNamed("CardView", owner: self, options: nil)
+                addSubview(contentView)
+                contentView.frame = self.bounds
+                contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+                let pointsTitle = NSLocalizedString("Points", comment: "User points in the game")
+                self.pointsLabel?.text = pointsTitle
+            }
+            
+            
+            @IBAction func firstCountrySelected(_ sender: UIButton) {
+                let firstFlagWidth = firstCountryWidth.constant
+                let secondFlagWidth = secondCountryWidth.constant
+                
+                if isFirstCountrySelected == false && isSecondCountrySelected == false {
+                    isFirstCountrySelected = !isFirstCountrySelected
+                    secondCountryButton.backgroundColor = .white
+                    secondCountryButton.alpha = 0.6
+                    
+                    firstCountryWidth.constant = firstFlagWidth * widthMultiplier
+                    
+                } else if isFirstCountrySelected == true && isSecondCountrySelected == false {
+                    isFirstCountrySelected = !isFirstCountrySelected
+                    secondCountryButton.backgroundColor = .clear
+                    secondCountryButton.alpha = 1.0
+                    
+                    firstCountryWidth.constant = firstFlagWidth / widthMultiplier
+                    
+                } else if isFirstCountrySelected == false && isSecondCountrySelected == true {
+                    isFirstCountrySelected = !isFirstCountrySelected
+                    isSecondCountrySelected = !isSecondCountrySelected
+                    firstCountryButton.backgroundColor = .clear
+                    firstCountryButton.alpha = 1.0
+                    
+                    secondCountryButton.backgroundColor = .white
+                    secondCountryButton.alpha = 0.6
+                    
+                    firstCountryWidth.constant = firstFlagWidth * widthMultiplier
+                    secondCountryWidth.constant = secondFlagWidth / widthMultiplier
+                }
+            
+            }
+            
+            
+            @IBAction func secondCountrySelected(_ sender: UIButton) {
+                let firstFlagWidth = firstCountryWidth.constant
+                let secondFlagWidth = secondCountryWidth.constant
+                
+                if isFirstCountrySelected == false && isSecondCountrySelected == false {
+                    isSecondCountrySelected = !isSecondCountrySelected
+                    firstCountryButton.backgroundColor = .white
+                    firstCountryButton.alpha = 0.6
+                    
+                    secondCountryWidth.constant = secondFlagWidth * widthMultiplier
+                    
+                } else if isFirstCountrySelected == false && isSecondCountrySelected == true {
+                    isSecondCountrySelected = !isSecondCountrySelected
+                    firstCountryButton.backgroundColor = .clear
+                    firstCountryButton.alpha = 1.0
+                    
+                    secondCountryWidth.constant = secondFlagWidth / widthMultiplier
+                    
+                } else if isFirstCountrySelected == true && isSecondCountrySelected == false {
+                    isSecondCountrySelected = !isSecondCountrySelected
+                    isFirstCountrySelected = !isFirstCountrySelected
+                    secondCountryButton.backgroundColor = .clear
+                    secondCountryButton.alpha = 1.0
+                    
+                    firstCountryButton.backgroundColor = .white
+                    firstCountryButton.alpha = 0.6
+                    
+                    firstCountryWidth.constant = firstFlagWidth / widthMultiplier
+                    secondCountryWidth.constant = secondFlagWidth * widthMultiplier
+                }
+            }
+            
+            func configure(with viewModel: ViewModel) {
+                titleLabel?.text = viewModel.title
+                subtitleLabel?.text = viewModel.subtitle
+                numberOfPointsLabel?.text = viewModel.numberOfPoints
+                firstCountryLabel?.text = viewModel.firstCountryName
+                secondCountryLabel?.text = viewModel.secondCountryName
+                dayLabel?.text = viewModel.day
+                monthLabel?.text = viewModel.mounth
+                timeLabel?.text = viewModel.time
+                
+                cornerImage?.image = viewModel.cornerImage
+                firstCountryImage?.image = viewModel.firstCountryFlag
+                secondCountryImage?.image = viewModel.secondCountryFlag
+            }
+            @IBAction func firstCountryPressed(_ sender: Any) {
+                firstCountryButton.isAccessibilityElement = true
+                if isFirstCountrySelected == false{
+                    firstCountryButton?.accessibilityHint = String(format: NSLocalizedString("You throbbed at Brazil", comment: ""))
+                } else {
+                    firstCountryButton?.accessibilityHint = String(format: NSLocalizedString("Double tap to throb at Brazil", comment: ""))
+                }
+            }
+            @IBAction func secondCountryPressed(_ sender: Any) {
+                secondCountryButton.isAccessibilityElement = true
+                if isSecondCountrySelected == false{
+                    firstCountryButton?.accessibilityHint = String(format: NSLocalizedString("You throbbed at China", comment: ""))
+                } else {
+                    firstCountryButton?.accessibilityHint = String(format: NSLocalizedString("Double tap to throb at China", comment: ""))
+                }
+                
+            }
         }
-    }
 
-    extension CardView {
-        struct ViewModel {
-            let title: String
-            let subtitle: String
-            let numberOfPoints: String
-            let firstCountryName: String
-            let secondCountryName: String
-            let day: String
-            let mounth: String
-            let time: String
-            let cornerImage: UIImage
-            let firstCountryFlag: UIImage
-            let secondCountryFlag: UIImage
+        extension CardView {
+            struct ViewModel {
+                let title: String
+                let subtitle: String
+                let numberOfPoints: String
+                let firstCountryName: String
+                let secondCountryName: String
+                let day: String
+                let mounth: String
+                let time: String
+                let cornerImage: UIImage
+                let firstCountryFlag: UIImage
+                let secondCountryFlag: UIImage
+            }
         }
-    }
